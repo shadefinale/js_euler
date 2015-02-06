128da6a34792da5e0356f3a2fd61c7001701f437
shadefinale <shadefinale@gmail.com>
2015-02-05 22:44:48 -0600

    Getting close to a solution to the 12th project euler problem that doesn't hang in browser.

--- euler12.js
+++ euler12.js
@@ -1,14 +1,15 @@
 function euler12_triangle(){
     var answer = 0;
-    var counter = 1;
-    var current_number = 1;
+    var counter = 0;
+    var current_number = 0;
     while (answer == 0){
-        if (count_divisors(current_number) >= 500) {
+        counter += 1;
+        current_number += counter;
+        if (count_divisors(current_number) > 500) {
             answer = current_number;
             break;
         }
-        counter += 1;
-        current_number += counter;
+
     }
 
     return answer;
@@ -16,31 +17,50 @@ function euler12_triangle(){
 
 // A helper function to check the amount of divisors a number has.
 function count_divisors(num){
-    if (num % 2 == 0){
-        num = num / 2;
-    }
+    var factors = {},
+        numFactors = 2,
+        currNum = num,
+        currFactor = 2;
 
-    var count = 0;
-    
-    while (num % 2 == 0){
-        count += 1;
-        num = num / 2;
-    }
-
-    var div_count = count + 1,
-        p = 3;
-
-    while (num != 1){
-        count = 0;
-        while(num % p == 0){
-            count += 1;
-            num = num / p;
+    while (!check_prime(currNum)){
+        if (currNum % currFactor !== 0){
+            if (!factors[currFactor]){
+                factors[currFactor] = 1;
+            } else {
+                factors[currFactor] += 1;
+            }
+        } else {
+            currFactor += 1;
         }
-        div_count = div_count * (count + 1);
-        p += 2;
+    }
+
+    if (!factors[currNum]){
+        factors[currNum] = 1;
+    } else {
+        factors[currNum] += 1;
+    }
+
+    div_count = 1;
+    
+    for (var key in factors){
+        div_count *= factors[key] + 1;
     }
 
     return div_count;
+ 
+}
+
+function check_prime(num){
+    if ((num % 2) == 0){
+        return false;
+    }
+
+    for (var i = 3; i < (Math.pow(num, .5) + 1); i += 2){
+        if ((num % i) == 0){
+            return false;
+        }
+    }
+    return true;
 }
 
 function init(){
